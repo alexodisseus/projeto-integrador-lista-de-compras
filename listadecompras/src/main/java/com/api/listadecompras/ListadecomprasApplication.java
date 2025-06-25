7a72ebe0d6a583f08977114ef7c03284da589ab5
alexodisseus <alexodisseus@gmail.com>
2025-06-25 09:47:24 -0300

    Projeto integrador lista de compras - etapa 7 e 8

---
+++ listadecompras/src/main/java/com/api/listadecompras/ListadecomprasApplication.java
@@ -0,0 +1,13 @@
+package com.api.listadecompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListadecomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListadecomprasApplication.class, args);
+	}
+
+}
